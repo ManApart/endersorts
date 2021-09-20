1c315f1eb6943247b70c91ec6b5e41f8c63fc453
ManApart <r@austinkucera.com>
2021-09-20 08:09:47 -0400

    additional fixes

--- src/main/java/org/manapart/endersort/Endersort.kt
+++ src/main/java/org/manapart/endersort/Endersort.kt
@@ -4,14 +4,23 @@ import net.minecraft.item.Item
 import net.minecraft.tileentity.TileEntityType
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent
+import org.apache.logging.log4j.LogManager
+import thedarkcolour.kotlinforforge.forge.FORGE_BUS
+import thedarkcolour.kotlinforforge.forge.MOD_BUS
 
 const val MODID = "endersort"
 
 @Mod(MODID)
 object Endersort {
+    private val log = LogManager.getLogger()
 
     init {
-        MinecraftForge.EVENT_BUS.register(this)
+        log.info("Endersort init")
+        ModBlocks.REGISTRY.register(MOD_BUS)
+        ModItems.REGISTRY.register(MOD_BUS)
+
+        FORGE_BUS.addListener(::onServerAboutToStart)
     }
 
     val tileType = createEntityType()
@@ -22,7 +31,9 @@ object Endersort {
             it.setRegistryName("$MODID:es_icon")
         }
     }
-
+    private fun onServerAboutToStart(event: FMLServerAboutToStartEvent) {
+        log.info("Server starting...")
+    }
 
     private fun createEntityType(): TileEntityType<EndersortEntity> {
         return TileEntityType.Builder.of({ EndersortEntity() }, ModBlocks.ENDERSORT_BLOCK)
