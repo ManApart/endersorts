edb4ac91cdbd5b602cc4cc2543d28c8bcaad1703
ManApart <r@austinkucera.com>
2022-01-17 15:10:18 -0500

    wip

--- src/main/java/org/manapart/endersort/ModItems.kt
+++ src/main/java/org/manapart/endersort/ModItems.kt
@@ -2,16 +2,18 @@ package org.manapart.endersort
 
 import net.minecraftforge.registries.DeferredRegister
 import net.minecraftforge.registries.ForgeRegistries
+import org.manapart.endersort.ModBlocks.ENDERSORT_BLOCK
+import org.manapart.endersort.ModBlocks.ENDER_EXTENDER_BLOCK
 import thedarkcolour.kotlinforforge.forge.registerObject
 
 object ModItems {
     val REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, MODID)
 
     val ENDERSORT_ITEM by REGISTRY.registerObject("endersort_item") {
-        EndersortItem(ModBlocks.endersortBlock)
+        EndersortItem(ENDERSORT_BLOCK)
     }
 
     val ENDER_EXTENDER_ITEM by REGISTRY.registerObject("ender_extender_item") {
-        EnderExtenderItem(ModBlocks.enderExtenderBlock)
+        EnderExtenderItem(ENDER_EXTENDER_BLOCK)
     }
 }
