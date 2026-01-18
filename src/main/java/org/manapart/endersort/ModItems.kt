51f20f15e8a384ee2d6e37cbc5cdf66b0778b168
ManApart <r@austinkucera.com>
2026-01-18 09:07:08 -0500

    initial gradle copy

--- src/main/java/org/manapart/endersort/ModItems.kt
+++ src/main/java/org/manapart/endersort/ModItems.kt
@@ -1,17 +1,34 @@
 package org.manapart.endersort
 
-import net.minecraftforge.registries.DeferredRegister
-import net.minecraftforge.registries.ForgeRegistries
-import thedarkcolour.kotlinforforge.forge.registerObject
+import net.minecraft.core.Registry
+import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.core.registries.Registries
+import net.minecraft.resources.ResourceKey
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.item.Item
+
 
 object ModItems {
-    val REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, MODID)
 
-    val ENDERSORT_ITEM by REGISTRY.registerObject("endersort_item") {
-        EndersortItem(ModBlocks.ENDERSORT_BLOCK)
+    fun <GenericItem : Item?> register(name: String?, itemFactory: (Item.Properties) -> GenericItem, settings: Item.Properties): GenericItem? {
+        // Create the item key.
+        val itemKey: ResourceKey<Item?> = ResourceKey.create(Registries.ITEM, ResourceLocation(MODID, name))
+
+        val item: GenericItem = itemFactory(settings.setId(itemKey))
+
+        // Register the item.
+        Registry.register(BuiltInRegistries.ITEM, itemKey, item)
+
+        return item
     }
 
-    val ENDER_EXTENDER_ITEM by REGISTRY.registerObject("ender_extender_item") {
-        EnderExtenderItem(ModBlocks.ENDER_EXTENDER_BLOCK)
-    }
+//    val REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, MODID)
+//
+//    val ENDERSORT_ITEM by REGISTRY.registerObject("endersort_item") {
+//        EndersortItem(ModBlocks.ENDERSORT_BLOCK)
+//    }
+//
+//    val ENDER_EXTENDER_ITEM by REGISTRY.registerObject("ender_extender_item") {
+//        EnderExtenderItem(ModBlocks.ENDER_EXTENDER_BLOCK)
+//    }
 }
